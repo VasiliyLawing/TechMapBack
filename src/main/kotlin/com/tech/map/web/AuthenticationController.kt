100772da0e75db3bb9cf0f77adb963f8284043f5
Cody McCodePants <cody@mccodepants.com>
2024-01-21 14:26:12 -0600

    Fixed Mistake

--- src/main/kotlin/com/tech/map/web/AuthenticationController.kt
+++ src/main/kotlin/com/tech/map/web/AuthenticationController.kt
@@ -1,7 +1,7 @@
 package com.tech.map.web
 
 import com.tech.map.config.AuthService
-import com.tech.map.dto.UserLoginRequest
+import com.tech.map.dto.ReqRes
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.*
 class AuthenticationController(@Autowired private val authService: AuthService) {
 
     @PostMapping("/register/")
-    fun signUp(@RequestBody signUpRequest: UserLoginRequest): ResponseEntity<UserLoginRequest> = ResponseEntity.ok(authService.signUp(signUpRequest))
+    fun signUp(@RequestBody signUpRequest: ReqRes): ResponseEntity<ReqRes> = ResponseEntity.ok(authService.signUp(signUpRequest))
 
     @PostMapping("/login/")
-    fun signIn(@RequestBody signInRequest: UserLoginRequest): ResponseEntity<UserLoginRequest> = ResponseEntity.ok(authService.signIn(signInRequest))
+    fun signIn(@RequestBody signInRequest: ReqRes): ResponseEntity<ReqRes> = ResponseEntity.ok(authService.signIn(signInRequest))
 
     @PostMapping("/refresh/")
-    fun refreshToken(@RequestBody refreshTokenRequest: UserLoginRequest): ResponseEntity<UserLoginRequest> = ResponseEntity.ok(authService.refreshToken(refreshTokenRequest))
+    fun refreshToken(@RequestBody refreshTokenRequest: ReqRes): ResponseEntity<ReqRes> = ResponseEntity.ok(authService.refreshToken(refreshTokenRequest))
 
 }
