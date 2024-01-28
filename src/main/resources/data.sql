2a9c5165b86c5661982818bcbe3b31c372d5ac73
Cody McCodePants <cody@mccodepants.com>
2024-01-28 14:43:42 -0600

    Added Field and normalized everything, will test with frontend,
    Fixed Company normalize Level 3

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,19 @@
-INSERT INTO students (name, latitude, longitude, field_id) VALUES
-                                                            ('Test', 42.392574068021005, -87.97722454804106, 0),
-                                                            ('Billy', 42.38083525124947, -87.98666242954418,  1);
+INSERT INTO fields (id, name) VALUES
+                                  (0, 'Cyber'),
+                                  (1, 'Cooking');
 
+INSERT INTO students (name, latitude, longitude, field_id) VALUES
+                                                               ('Test', 42.392574068021005, -87.97722454804106, 1),
+                                                               ('Billy', 42.38083525124947, -87.98666242954418,  1);
 
 
 INSERT INTO companies (name, latitude, longitude) VALUES
                                                             ('Apple', 42.38083525124947, -87.95866242954418),
                                                             ('Apple', 42.38083525124947, -87.95866242954418);
 
- INSERT INTO field_company (company_id, field_id) VALUES
-                                                      (1, 0);
+INSERT INTO field_company (company_id, field_id) VALUES
+    (1, 1);
 
-INSERT INTO users (username, password, role) VALUES
-                                       ('admin', 'password', 'ADMIN');
-
-
---     ('NAme', 42.38083525124947, -87.98866242954418,  'Medical'),
---     ('Apple', 42.38083525124947, -87.95866242954418,  'Cyber'),
---     new Student("NAme", 42.38083525124947, -87.98866242954418,  "CyberSecurity")
-INSERT INTO fields (id, name) VALUES
-                                  (0, 'Cyber'),
-                                  (1, 'Cooking');
+-- INSERT INTO users (username, password, role) VALUES
+--     ('admin', 'password', 'ADMIN');
+--
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,47 +1,47 @@
 DROP  TABLE IF EXISTS "fields" CASCADE ;
 
-CREATE TABLE "fields" (
-                          id BIGSERIAL PRIMARY KEY,
-                          "name" text
-);
-
-
 DROP TABLE IF EXISTS "students";
-
-CREATE TABLE "students" (
-                            id SERIAL PRIMARY KEY,
-                            "name" text,
-                            "latitude" decimal,
-                            "longitude" decimal,
-                            "field_id" BIGINT,
-                            FOREIGN KEY (field_id) REFERENCES fields(id)
-);
 
 DROP TABLE IF EXISTS "companies" CASCADE ;
 
-CREATE TABLE "companies" (
-                             id SERIAL PRIMARY KEY,
-                             "name" text,
-                             "latitude" decimal,
-                             "longitude" decimal
-                         );
-
 DROP TABLE IF EXISTS "field_company" CASCADE ;
-CREATE TABLE field_company (
-                               field_id BIGINT,
-                               company_id BIGINT,
-                               FOREIGN KEY (field_id) REFERENCES fields(id),
-                               FOREIGN KEY (company_id) REFERENCES companies(id),
-                               PRIMARY KEY (field_id, company_id)
-);
-
 
 DROP TABLE IF EXISTS "users";
 
-CREATE TABLE "users" (
-                         id SERIAL PRIMARY KEY,
-                         "username" text,
-                         "password" text,
-                         "role" text
-                     );
+CREATE TABLE fields (
+                        id BIGSERIAL PRIMARY KEY,
+                        name TEXT
+);
 
+CREATE TABLE students (
+                          id SERIAL PRIMARY KEY,
+                          name TEXT,
+                          latitude DOUBLE PRECISION,
+                          longitude DOUBLE PRECISION,
+                          field_id BIGINT,
+                          FOREIGN KEY (field_id) REFERENCES fields(id)
+);
+
+CREATE TABLE companies (
+                           id SERIAL PRIMARY KEY,
+                           name TEXT,
+                           latitude DOUBLE PRECISION,
+                           longitude DOUBLE PRECISION
+);
+
+CREATE TABLE field_company (
+                               company_id BIGINT,
+                               field_id BIGINT,
+
+                               FOREIGN KEY (company_id) REFERENCES companies(id),
+                               FOREIGN KEY (field_id) REFERENCES fields(id),
+                               PRIMARY KEY (company_id, field_id)
+);
+
+
+CREATE TABLE users (
+                       id SERIAL PRIMARY KEY,
+                       username TEXT,
+                       password TEXT,
+                       role TEXT
+);
