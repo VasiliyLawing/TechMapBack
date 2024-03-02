9b5aeafb1cf2dc14b1cda64a4a84de6ffd15c857
Cody McCodePants <cody@mccodepants.com>
2024-03-02 16:26:23 -0600

    Cors

---
+++ src/main/kotlin/com/tech/map/config/CorsConfig.kt
@@ -0,0 +1,24 @@
+package com.tech.map.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("https://techmapfrontend.web.app")
+        config.addAllowedOrigin("https://humble-eureka-jxw7q59jqjphq7vr-4200.app.github.dev")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
+    }
+}
